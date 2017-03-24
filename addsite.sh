6c746da1ce6df1f6eb3e59e3bd253ce760163e75
Francis Dubreuil <acquisti@rahona.be>
2017-03-24 19:32:35 +0100

    fixed fcgi://domain in apache2 vhost

--- addsite.sh
+++ addsite.sh
@@ -1,7 +1,6 @@
-#Simple script to create php-fpm pool, related user and apache2 vhost.
-
-
+#Simple script to create php-fpm pool and user, and apache2 vhost.
 #!/bin/bash
+
 if [ $# -ne 2 ]; then
         echo "Usage: $0 domain tld. For example $0 arielcaldaie it for arielcaldaie.it"
         exit 1;
@@ -14,5 +13,6 @@ adduser www-data $1
 sed s/template/$1/ template-fpm > /etc/php5/fpm/pool.d/$1.conf
 echo "All done with php5-fpm. Please review information in /etc/php5/fpm/pool.d/$1.conf before reload php5-fpm"
 sed s/domain/$1/ template-apache > /etc/apache2/sites-available/$1.conf
-sed -i s/tld/$2/ /etc/apache2/sites-available/$1.conf
+sed -i "s/\/domain/\/$1/" /etc/apache2/sites-available/$1.conf 
+sed -i "s/tld/$2/" /etc/apache2/sites-available/$1.conf
 echo "All done with apache2. Please review information in /etc/apache2/sites-available/$1.conf before a2ensite $1 and reload apache"
