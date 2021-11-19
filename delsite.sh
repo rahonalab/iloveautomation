2229107eb2384e4441b2abea91d740720546f325
root <luigi@rahona.be>
2021-11-19 13:24:41 +0100

    delsite

--- delsite.sh
+++ delsite.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-if [ $# -ne 2 ]; then
+if [ $# -ne 3 ]; then
         echo "Usage: $0 2ndleveldomain php_version. For example $0 arielcaldaie 7.1 for arielcaldaie.it"
         exit 1;
 fi
 
-rm -fv /etc/php/$2/fpm/pool.d/$1.conf
+rm -fv /etc/php/$3/fpm/pool.d/$1.conf
 rm -fv /etc/apache2/sites-enabled/$1.conf
 rm -fv /etc/apache2/sites-available/$1.conf
-/etc/init.d/php$2-fpm restart
+rm -rfv /var/www/$1.$2
+/etc/init.d/php$3-fpm restart
 /etc/init.d/apache2 restart
-deluser --remove-all-files --remove-home $1
+deluser --remove-home $1
 groupdel $1
